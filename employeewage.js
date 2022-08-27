782ec85641d3d8a2ae0a64ef3c513edbfa1664ba
unknown <suntanabhagyashree@gmail.com>
2022-08-27 10:40:26 +0530

    ADDED:Calculating Wages till a condition of total working hours

--- employeewage.js
+++ employeewage.js
@@ -1,8 +1,13 @@
-const NUM_OF_WORKING_DAYS = 2;
-let empHrs = 0;
-for(let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
-let empCheck = Math.floor(Math.random() * 10) % 3;
-empHrs += getWorkingHours(empCheck);
-}
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Total Hrs: " +empWage+ "Emp Wage: " + empWage);
+const MAX_HRS_IN_MONTH = 100;
+const NUM_OF_WORKING_DAYS = 10;
+let totalEmpHrs= 0;
+let totalWorkingDays = 0;
+while (totalEmpHrs <= MAX_HRS_IN_MONTH &&
+        totalWorkingDays < NUM_OF_WORKING_DAYS) {
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+        totalEmpHrs += getWorkingHours(empCheck);
+ }
+let empWage = totalEmpHrs * WAGE_PER_HOUR;
+console.log("UC5 - Total Days: " + totalWorkingDays +
+            "Total Hrs: " + totalEmpHrs + " Emp Wage: " + empWage);
