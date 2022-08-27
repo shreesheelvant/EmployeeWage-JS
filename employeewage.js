667c852c80bf6c24f1ae91a497b061dcf000a252
unknown <suntanabhagyashree@gmail.com>
2022-08-27 11:01:50 +0530

    ADDED:Store the Daily Wage along with the total wage

--- employeewage.js
+++ employeewage.js
@@ -1,13 +1,20 @@
-const MAX_HRS_IN_MONTH = 100;
+function calcDailyWage(empHrs) {
+    return empHrs * WAGE_PER_HOUR;
+}
+const MAX_HRS_IN_MONTH = 160;
 const NUM_OF_WORKING_DAYS = 10;
 let totalEmpHrs= 0;
 let totalWorkingDays = 0;
+let empDailyWageArr = new Array();
+
 while (totalEmpHrs <= MAX_HRS_IN_MONTH &&
         totalWorkingDays < NUM_OF_WORKING_DAYS) {
     totalWorkingDays++;
     let empCheck = Math.floor(Math.random() * 10) % 3;
-        totalEmpHrs += getWorkingHours(empCheck);
- }
-let empWage = totalEmpHrs * WAGE_PER_HOUR;
-console.log("UC5 - Total Days: " + totalWorkingDays +
+    let empHrs = getWorkingHours(empCheck);
+    totalEmpHrs += empHrs;
+    empDailyWageArr.push(calcDailyWage(totalEmpHrs));
+    }
+let empWage = calcDailyWage(totalEmpHrs);
+console.log("UC6 - Total Days: " + totalWorkingDays +
             "Total Hrs: " + totalEmpHrs + " Emp Wage: " + empWage);
