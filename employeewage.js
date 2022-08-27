54cca01abd234bab07b1bf6b0dfd1ef1d93e2c0f
unknown <suntanabhagyashree@gmail.com>
2022-08-27 10:13:14 +0530

    ADDED:Refactor the code to write a function to get work hours

--- employeewage.js
+++ employeewage.js
@@ -1,21 +1,15 @@
-const IS_PART_TIME = 1;
-const IS_FULL_TIME = 2;
-const PART_TIME_HOURS = 4;
-const FULL_TIME_HOURS= 8;
-const WAGE_PER_HOUR=20;
-
-let empHrs = 0;
-empCheck = Math.floor(Math.random() * 10) % 3;
-switch (empCheck) {
-    case IS_PART_TIME: 
-    empHrs = PART_TIME_HOURS;
-    break;
-    case IS_FULL_TIME:
-        empHrs = FULL_TIME_HOURS;
-        break;
+function getWorkingHours(empCheck) {
+    switch (empCheck) {
+        case IS_PART_TIME:
+          return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+          return FULL_TIME_HOURS;
         default:
-        empHrs = 0;
+        return 0;
+    }
 }
+let empHrs = 0;
+let empCheck = Math.floor(Math.random() * 10) % 3;
+empHrs = getWorkingHours(empCheck);
 let empWage = empHrs * WAGE_PER_HOUR;
-    console.log("Emp Wage:" + empWage);
-
+console.log("Emp Wage: " + empWage);
